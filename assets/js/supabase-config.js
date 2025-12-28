e6901210ca2cab8179aae9a83059909f8ff38de8
Harshit choudhary <harshitc.cs.22@nitj.ac.in>
2025-12-29 05:10:26 +0530

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,10 +1,10 @@
 // assets/js/supabase-config.js
 
 // 1. YOUR SUPABASE KEYS
-const SUPABASE_URL = 'https://fpwudmcrvgknsdtowrqb.supabase.co';
+const SUPABASE_URL = 'https://fgzlekquexmtnzrhjswd.supabase.co';
 
 // 🔴 IMPORTANT: Copy the full key from your dashboard and paste it inside the quotes below
-const SUPABASE_ANON_KEY = 'sb_publishable_I60QCfCHJVTtWEWvghs34g_Ce2aJ6d8'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnemxla3F1ZXhtdG56cmhqc3dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5NDgxNTQsImV4cCI6MjA4MjUyNDE1NH0.hEDqZzidfJJTE5n0KF2Jd1XNMSbDyZcut4MP-PCi1NY'; 
 
 // 2. Initialize Supabase
 let supabaseClient;
